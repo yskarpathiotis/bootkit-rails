a8836208d30492678ca35d6a0030f273f6cf49bc
Yiannis Skarpathiotis <yiannis@noventech.com>
2025-02-28 19:51:22 -0600

    make updates for Kamal deployment

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,13 @@ set -e
 # Install/update gems
 bundle install
 
+# ignore issues between local user and docker user when running git commands
+git config --global --add safe.directory /rails/.git
+
+# set permissions to access ssh files for kamal
+chown -R root:root /root/.ssh
+chmod 700 /root/.ssh
+chmod 600 /root/.ssh/*
+
 # Execute the main command
 exec "$@"
